1e87d5366e593cc6da87a5b76c90d75bff7f50c6
Suraj Yadav <surajyadav278190@gmail.com>
2026-08-30 17:28:51 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,43 @@
+class MinStack {
+    Stack<Long> st;
+    long min;
+
+    public MinStack() {
+        st = new Stack<Long>();
+        
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push(0L);
+            min = val;
+        }else{
+            st.push(val-min);
+            if(val<min){
+                min=val;
+            }
+        }
+
+        
+    }
+    
+    public void pop() {
+        long pop = st.pop();
+
+        if(pop<0) min = min - pop;
+    }
+    
+    public int top() {
+       long top = st.peek();
+       if(top>0){
+        return (int) (min+top);
+       }else{
+        return (int ) min;
+       }
+        
+    }
+    
+    public int getMin() {
+        return (int) min;
+    }
+}
